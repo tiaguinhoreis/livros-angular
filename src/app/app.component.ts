ef435d2a05bb4cbfbe81dc4d6e79f61f75f4a130
TiaguinhoReis <tiagofdr@hotmail.com>
2024-11-05 23:23:05 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-livro-lista',
   standalone: true ,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'livros-angular';
+  title = 'Livros - Angular';           
 }
